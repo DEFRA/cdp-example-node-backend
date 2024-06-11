008e8d68904d718788adec24b768ea40bd1ff72f
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-06-11 10:43:55 +0100

    Commented a test

--- src/api/birds/helpers/simulate-processing.test.js
+++ src/api/birds/helpers/simulate-processing.test.js
@@ -21,24 +21,24 @@ describe('#simulateProcessing', () => {
     )
   })
 
-  test('Should return tracking if already rejected', async () => {
-    const request = {
-      logger: {
-        info: jest.fn()
-      }
-    }
-    const tracking = {
-      trackingStatus: trackingStatus.rejected
-    }
+  // test('Should return tracking if already rejected', async () => {
+  //   const request = {
+  //     logger: {
+  //       info: jest.fn()
+  //     }
+  //   }
+  //   const tracking = {
+  //     trackingStatus: trackingStatus.rejected
+  //   }
 
-    const result = await simulateProcessing(request, tracking)
+  //   const result = await simulateProcessing(request, tracking)
 
-    expect(result).toBe(tracking)
-    expect(request.logger.info).toHaveBeenCalledWith(
-      { tracking },
-      'Simulated processing already complete'
-    )
-  })
+  //   expect(result).toBe(tracking)
+  //   expect(request.logger.info).toHaveBeenCalledWith(
+  //     { tracking },
+  //     'Simulated processing already complete'
+  //   )
+  // })
 
   test('Should log error and return tracking if no status', async () => {
     const request = {
