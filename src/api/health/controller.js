1f79590c5e8d654a8fe99430d474d11cb8a6ba54
chotai <chotai@users.noreply.github.com>
2024-10-15 14:19:57 +0100

    Revert: Intentionally fail healthcheck with process.exit (testing)

--- src/api/health/controller.js
+++ src/api/health/controller.js
@@ -1,8 +1,5 @@
 const healthController = {
-  handler: (request, h) => {
-    process.exit(1)
-    return h.response({ message: 'success' }).code(200)
-  }
+  handler: (request, h) => h.response({ message: 'success' }).code(200)
 }
 
 export { healthController }
