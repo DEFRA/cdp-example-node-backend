b24e3cbdcb01a8b74e26fa8c85f0c134c0b1e532
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-05-28 17:28:34 +0100

    testing without proxy

--- src/api/example/controllers/call-public.js
+++ src/api/example/controllers/call-public.js
@@ -1,8 +1,6 @@
-import { proxyFetch } from '~/src/helpers/proxy-fetch'
-
 export const callPublic = {
   handler: async (request, h) => {
-    const res = await proxyFetch(
+    const res = await fetch(
       'https://cdp-defra-id-stub.dev.cdp-int.defra.cloud/cdp-defra-id-stub/.well-known/openid-configuration'
     )
     return h.response('ok ' + res.status).code(200)
